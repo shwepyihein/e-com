95b0da6935e1e562532349a9b0e95b26c0873612
shwepyihein <shwepyihein.12@gmail.com>
2020-05-15 21:00:43 +0630

    partice react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Homepage from './Hompage'
+function App() {
+  return (
+    <div>
+        <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hompage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Homepage.scss'
+
+
+const Homepage =()=>{
+    return(
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
