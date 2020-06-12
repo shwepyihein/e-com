db47ca7f4841df643e04ed03bc7fb4879cef70b5
shwepyihein <shwepyihein.12@gmail.com>
2020-06-13 01:03:17 +0630

    created cart-item component and items find function

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 import './cart-dropdown.scss';
+import { connect } from 'react-redux'
 import CustomButtom from '../../components/custom-buttom/custom-buttom';
-
-const CartDropdown= ()=>{
+import CartItem from '../cart-item/cart-item'
+const CartDropdown= ({cartItems})=>{
     return(
         <div className='cart-dropdown'>
             <div className="cart-items">
-                
+                {
+                    cartItems.map(cartItem=><CartItem key={cartItem.id} item={cartItem}/>)
+                }
             </div>
             <CustomButtom> Go To Checkout</CustomButtom>
         </div>
     )
 }
-
-export default CartDropdown
+const mapStateToProps =({cart:{cartItems}})=>({
+    cartItems
+})
+export default connect(mapStateToProps)(CartDropdown)
